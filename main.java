24dba880be7065c58761f3ef12677c511deca3fb
kalagau <gauravjeet.kala@mail.utoronto.ca>
2016-07-10 02:37:10 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+import java.io.*;
+public class Main
+{
+   
+   private static Scanner sc = new Scanner(System.in);
+   private static BufferedReader input;
+   private static String buffer;
+   public static void main (String[] args)
+   {
+      
+       
+      //initilize input buffer
+      buffer = "";
+       
+   }
+}
